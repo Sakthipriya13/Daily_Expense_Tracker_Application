2a6954ba2b449a30241cdb6121e580d90325844f
Sakthipriya13 <sakthipriyaravi2019@gmail.com>
2026-05-12 22:43:16 +0530

    Commit On "May 12"

--- app/src/main/java/com/example/expensetrackerapplication/worker/SyncWorker.kt
+++ app/src/main/java/com/example/expensetrackerapplication/worker/SyncWorker.kt
@@ -3,6 +3,7 @@ package com.example.expensetrackerapplication.worker
 import android.app.Application
 import android.content.Context
 import android.util.Log
+import androidx.paging.LOG_TAG
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import com.example.expensetrackerapplication.data.database.AppDatabase
@@ -10,17 +11,22 @@ import com.example.expensetrackerapplication.data.repositary.CategoryRepository
 import com.example.expensetrackerapplication.data.repositary.ExpenseRepository
 import com.example.expensetrackerapplication.data.repositary.IncomeRepository
 import com.example.expensetrackerapplication.data.repositary.UserRepository
+import com.example.expensetrackerapplication.logger.FileLogger
 import com.example.expensetrackerapplication.utils.Global
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.tasks.await
 
-class SyncWorker(context: Context, workerParams: WorkerParameters)
-    : CoroutineWorker(context, workerParams) {
+class SyncWorker(
+    val context: Context,
+    val workerParams: WorkerParameters,
+    val logger : FileLogger)
+    : CoroutineWorker(context, workerParams)
+{
+    val LOG_TAG = "SYNC_WORKER"
 
     override suspend fun doWork(): Result {
-
-        return try {
-
+        return try
+        {
             Log.i("SYNC_WORKER", "Sync Started")
 
             val firestore = FirebaseFirestore.getInstance()
@@ -35,7 +41,11 @@ class SyncWorker(context: Context, workerParams: WorkerParameters)
 
             Result.success()
 
-        } catch (e: Exception) {
+        }
+        catch (e: Exception)
+        {
+
+            logger.logError(LOG_TAG,"Do Work: ${e.message}")
 
             Log.e("SYNC_WORKER", "Sync Failed: ${e.message}")
 
@@ -46,74 +56,89 @@ class SyncWorker(context: Context, workerParams: WorkerParameters)
     private suspend fun fnSyncUser(
         db: AppDatabase,
         firestore: FirebaseFirestore
-    ) {
-        val repository =
-            UserRepository(db.userDao(), application = applicationContext as Application)
-        val users = repository.fnGetUnSyncedCurUserDetails()
+    )
+    {
+        try
+        {
+            val repository =
+                UserRepository(db.userDao(), application = applicationContext as Application,logger)
+            val users = repository.fnGetUnSyncedCurUserDetails()
 
-        Log.i("SYNC_USER", "Unsynced Users: ${users.size}")
+            Log.i("SYNC_USER", "Unsynced Users: ${users.size}")
 
-        for (u in users) {
+            for (u in users) {
 
-            val docRef = firestore
-                .collection("ExpenseTrackerUser")
-                .document(Global.cloudUserId)
+                val docRef = firestore
+                    .collection("ExpenseTrackerUser")
+                    .document(Global.cloudUserId)
 
-            val map = hashMapOf(
-                "userId" to u.userId,
-                "cloudId" to Global.cloudUserId,
-                "isSynced" to 1,
-                "signUpDate" to u.signUpDate,
-                "userName" to u.userName,
-                "userEmail" to u.userEmail,
-                "userMobileNo" to u.userMobileNo,
-                "userPassword" to u.userPassword
+                val map = hashMapOf(
+                    "userId" to u.userId,
+                    "cloudId" to Global.cloudUserId,
+                    "isSynced" to 1,
+                    "signUpDate" to u.signUpDate,
+                    "userName" to u.userName,
+                    "userEmail" to u.userEmail,
+                    "userMobileNo" to u.userMobileNo,
+                    "userPassword" to u.userPassword
 //                "userProfilePhotoUri" to u.userProfilePhotoUri
-            )
-            docRef.set(map).await()
+                )
+                docRef.set(map).await()
 
-            u.isSynced = 1
-            u.cloudId = docRef.id
+                u.isSynced = 1
+                u.cloudId = docRef.id
 
-            repository.fnUpdateCategoryDb(u)
+                repository.fnUpdateCategoryDb(u)
+            }
+
+        }
+        catch (e: Exception)
+        {
+            logger.logError(LOG_TAG,"Sync User: ${e.message}")
         }
     }
 
     private suspend fun fnSyncCategory(
         db: AppDatabase,
         firestore: FirebaseFirestore
-    ) {
+    )
+    {
+        try {
+            val repository = CategoryRepository(db.CategoryDao(),logger)
+            val categories = repository.fnGetUnSyncedCategoryDb()
 
-        val repository = CategoryRepository(db.CategoryDao())
-        val categories = repository.fnGetUnSyncedCategoryDb()
+            Log.i("SYNC_CATEGORY", "Unsynced Categories: UserId: ${Global.lUserId}")
 
-        Log.i("SYNC_CATEGORY", "Unsynced Categories: UserId: ${Global.lUserId}")
+            Log.i("SYNC_CATEGORY", "Unsynced Categories: Size : ${categories.size} And Categories: $categories")
 
-        Log.i("SYNC_CATEGORY", "Unsynced Categories: Size : ${categories.size} And Categories: $categories")
+            for (cat in categories) {
 
-        for (cat in categories) {
+                val docRef = firestore
+                    .collection("ExpenseTrackerUser")
+                    .document(Global.cloudUserId)
+                    .collection("Categories")
+                    .document()
 
-            val docRef = firestore
-                .collection("ExpenseTrackerUser")
-                .document(Global.cloudUserId)
-                .collection("Categories")
-                .document()
+                val map = hashMapOf(
+                    "userId" to cat.userId,
+                    "cloudId" to docRef.id,
+                    "isSynced" to 1,
+                    "categoryId" to cat.categoryId,
+                    "signUpDate" to cat.signUpDate,
+                    "categoryName" to cat.categoryName
+                )
 
-            val map = hashMapOf(
-                "userId" to cat.userId,
-                "cloudId" to docRef.id,
-                "isSynced" to 1,
-                "categoryId" to cat.categoryId,
-                "signUpDate" to cat.signUpDate,
-                "categoryName" to cat.categoryName
-            )
+                docRef.set(map).await()
 
-            docRef.set(map).await()
+                cat.isSynced = 1
+                cat.cloudId = docRef.id
 
-            cat.isSynced = 1
-            cat.cloudId = docRef.id
-
-            repository.fnUpdateCategoryDb(cat)
+                repository.fnUpdateCategoryDb(cat)
+            }
+        }
+        catch (e: Exception)
+        {
+            logger.logError(LOG_TAG,"Sync Category: ${e.message}")
         }
     }
 
@@ -121,79 +146,92 @@ class SyncWorker(context: Context, workerParams: WorkerParameters)
         db: AppDatabase,
         firestore: FirebaseFirestore
     ) {
+        try
+        {
+            val repository = ExpenseRepository(db.ExpenseDao(),logger)
+            val expenses = repository.fnGetUnSyncedExpense()
 
-        val repository = ExpenseRepository(db.ExpenseDao())
-        val expenses = repository.fnGetUnSyncedExpense()
+            Log.i("SYNC_EXPENSE", "Unsynced Expenses: ${expenses.size}")
 
-        Log.i("SYNC_EXPENSE", "Unsynced Expenses: ${expenses.size}")
+            for (expense in expenses) {
 
-        for (expense in expenses) {
+                val docRef = firestore
+                    .collection("ExpenseTrackerUser")
+                    .document(Global.cloudUserId)
+                    .collection("Expenses")
+                    .document()
 
-            val docRef = firestore
-                .collection("ExpenseTrackerUser")
-                .document(Global.cloudUserId)
-                .collection("Expenses")
-                .document()
+                val map = hashMapOf(
+                    "userId" to expense.userId,
+                    "cloudId" to docRef.id,
+                    "isSynced" to 1,
+                    "expenseId" to expense.expenseId,
+                    "expenseDate" to expense.expenseDate,
+                    "expenseAmt" to expense.expenseAmt,
+                    "expenseCategoryId" to expense.expenseCategoryId,
+                    "expenseCategoryName" to expense.expenseCategoryName,
+                    "paymentType" to expense.paymentType,
+                    "expenseAmtInCash" to expense.expenseAmtInCash,
+                    "expenseAmtInCard" to expense.expenseAmtInCard,
+                    "expenseAmtInUpi" to expense.expenseAmtInUpi,
+                    "expenseAmtInOthers" to expense.expenseAmtInOthers,
+                    "expenseRemarks" to expense.expenseRemarks,
+                    "expenseStatus" to expense.expenseStatus
+                )
 
-            val map = hashMapOf(
-                "userId" to expense.userId,
-                "cloudId" to docRef.id,
-                "isSynced" to 1,
-                "expenseId" to expense.expenseId,
-                "expenseDate" to expense.expenseDate,
-                "expenseAmt" to expense.expenseAmt,
-                "expenseCategoryId" to expense.expenseCategoryId,
-                "expenseCategoryName" to expense.expenseCategoryName,
-                "paymentType" to expense.paymentType,
-                "expenseAmtInCash" to expense.expenseAmtInCash,
-                "expenseAmtInCard" to expense.expenseAmtInCard,
-                "expenseAmtInUpi" to expense.expenseAmtInUpi,
-                "expenseAmtInOthers" to expense.expenseAmtInOthers,
-                "expenseRemarks" to expense.expenseRemarks,
-                "expenseStatus" to expense.expenseStatus
-            )
+                docRef.set(map).await()
 
-            docRef.set(map).await()
+                expense.isSynced = 1
+                expense.cloudId = docRef.id
 
-            expense.isSynced = 1
-            expense.cloudId = docRef.id
-
-            repository.fnUpdateExpense(expense)
+                repository.fnUpdateExpense(expense)
+            }
+        }
+        catch (e: Exception)
+        {
+            logger.logError(LOG_TAG,"Sync Expense: ${e.message}")
         }
     }
 
     private suspend fun fnSyncIncome(
         db: AppDatabase,
         firestore: FirebaseFirestore
-    ) {
-        val repository = IncomeRepository(db.IncomeDao())
-        val incomes = repository.fnGetUnSyncedIncomes()
+    )
+    {
+        try {
+            val repository = IncomeRepository(db.IncomeDao(),logger)
+            val incomes = repository.fnGetUnSyncedIncomes()
 
-        Log.i("SYNC_INCOME", "Unsynced Incomes: ${incomes.size}")
+            Log.i("SYNC_INCOME", "Unsynced Incomes: ${incomes.size}")
 
-        for (income in incomes) {
+            for (income in incomes) {
 
-            val docRef = firestore
-                .collection("ExpenseTrackerUser")
-                .document(Global.cloudUserId)
-                .collection("Incomes")
-                .document()
+                val docRef = firestore
+                    .collection("ExpenseTrackerUser")
+                    .document(Global.cloudUserId)
+                    .collection("Incomes")
+                    .document()
 
-            val map = hashMapOf(
-                "userId" to income.userId,
-                "cloudId" to docRef.id,
-                "isSynced" to 1,
-                "incomeId" to income.incomeId,
-                "date" to income.date,
-                "income" to income.income
-            )
+                val map = hashMapOf(
+                    "userId" to income.userId,
+                    "cloudId" to docRef.id,
+                    "isSynced" to 1,
+                    "incomeId" to income.incomeId,
+                    "date" to income.date,
+                    "income" to income.income
+                )
 
-            docRef.set(map).await()
+                docRef.set(map).await()
 
-            income.isSynced = 1
-            income.cloudId = docRef.id
+                income.isSynced = 1
+                income.cloudId = docRef.id
 
-            repository.fnUpdateIncome(income)
+                repository.fnUpdateIncome(income)
+            }
+        }
+        catch (e: Exception)
+        {
+            logger.logError(LOG_TAG,"Sync Income: ${e.message}")
         }
     }
 }
